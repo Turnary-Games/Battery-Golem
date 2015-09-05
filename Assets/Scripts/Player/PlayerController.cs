ddcd09e71e119390f30add4a2af6e564d5d08f41
kalle (jag) <kalle.jillheden@me.com>
2015-09-05 20:25:16 +0200

    Added basic player movement
    :small_blue_diamond: Added player movement
    :small_blue_diamond: Added player jumping
    :small_blue_diamond: Camera follows the player (smoothly)
    :small_blue_diamond: Player rotates in towards the walking direction
    
    :small_orange_diamond: Link to card: https://trello.com/c/BA4tA1Xo
    
    :small_red_triangle: TODO: Make the player be able to use "stuff".
    Activate "things" by electrifying/touching them.
    :small_red_triangle: TODO: Add an item slot for the player, so they can
    for example hold a fan, a lamp, a needle, etc.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	[Header("Variables (DONT ALTER)")]
+
+	public CharacterController character;
+
+	[Header("Movement settings")]
+
+	[Tooltip("Speed in meters per second.")]
+	public float moveSpeed = 1;
+	public float jumpHeight = 1;
+	public bool continuousJumping = true;
+
+	// Current movement velocity
+	private Vector3 motion;
+
+	void Update () {
+		Movement ();
+	}
+
+	void Movement() {
+		// Motion to apply to the character
+		motion = GetAxis () * moveSpeed + Vector3.up * motion.y;
+
+		if (character.isGrounded) {
+			// Jumping
+			if (IsJumping())
+				motion.y = jumpHeight;
+		} else {
+			// Apply gravity
+			motion += Physics.gravity * Time.deltaTime;
+		}
+
+		// Move the character
+		character.Move (motion * Time.deltaTime);
+
+		// Rotate the character
+		if (motion.x != 0 || motion.z != 0)
+			character.transform.eulerAngles = Vector3.up * (Mathf.Atan2 (motion.z, -motion.x) * Mathf.Rad2Deg - 90f);
+	}
+
+	Vector3 GetAxis() {
+		// Vector of the axis
+		Vector3 raw_axis = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+
+		// Normalize it so that all directions moves the same combined speed
+		Vector3 axis = raw_axis.normalized;
+
+		// Restore the movement lerp that's lost in the normalization
+		axis.x *= Mathf.Abs (raw_axis.x);
+		axis.z *= Mathf.Abs (raw_axis.z);
+
+		return axis;
+	}
+
+	bool IsJumping() {
+		return (continuousJumping && Input.GetButton ("Jump"))
+			|| (!continuousJumping && Input.GetButtonDown ("Jump"));
+	}
+}
