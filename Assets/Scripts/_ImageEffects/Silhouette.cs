e9a6b07b68dde2a055ac56e4c2ce777eb21d0119
kalle (jag) <kalle.jillheden@me.com>
2016-03-01 12:40:57 +0100

    Updated Unity
    + Updated Unity from 5.3.2f1 to 5.3.3f1.
    + Removed unused materials.
    + Fixed the silhouette shader (again...).

--- Assets/Scripts/_ImageEffects/Silhouette.cs
+++ Assets/Scripts/_ImageEffects/Silhouette.cs
@@ -6,7 +6,7 @@ public class Silhouette : MonoBehaviour {
 
 	public Color silhouetteColor = new Color(1,1,1,0.5f);
 	private Material material;
-	RenderTexture buffer;
+	//RenderTexture buffer;
 
 	void Awake() {
 		material = new Material(Shader.Find("Hidden/SilhouetteImageEffect"));
@@ -15,14 +15,15 @@ public class Silhouette : MonoBehaviour {
 	void OnRenderImage(RenderTexture source, RenderTexture destination) {
 		material.SetColor("_Silhouette", silhouetteColor);
 
-		if (buffer == null || buffer.width != source.width || buffer.height != source.height) {
-			buffer = RenderTexture.GetTemporary(source.width, source.height, 0);
-		}
+		//if (buffer == null || buffer.width != source.width || buffer.height != source.height) {
+		//	buffer = RenderTexture.GetTemporary(source.width, source.height, 0);
+		//}
 
-		Graphics.SetRenderTarget(buffer.colorBuffer, source.depthBuffer);
-		Graphics.Blit(source, material);
-		Graphics.Blit(buffer, destination);
-		//Graphics.Blit(source, destination, material);
+		//Graphics.SetRenderTarget(buffer.colorBuffer, source.depthBuffer);
+		//Graphics.Blit(source, material);
+		//Graphics.Blit(buffer, destination);
+
+		Graphics.Blit(source, destination, material);
 	}
 
 }
