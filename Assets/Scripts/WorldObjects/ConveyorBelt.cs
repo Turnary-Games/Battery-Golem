7a01dfc83e566414247b566a3e9feb3b35542efe
kalle (jag) <kalle.jillheden@me.com>
2016-04-25 18:05:54 +0200

    blerg

--- Assets/Scripts/WorldObjects/ConveyorBelt.cs
+++ Assets/Scripts/WorldObjects/ConveyorBelt.cs
@@ -7,19 +7,29 @@ public class ConveyorBelt : MonoBehaviour {
 
 	public Vector3 motion;
 
+	void OnCollisionEnter(Collision col) {
+		if (col.collider.attachedRigidbody != null) {
+			col.collider.attachedRigidbody.sleepThreshold = 0;
+		}
+	}
+
+	void OnCollisionExit(Collision col) {
+		if (col.collider.attachedRigidbody != null) {
+
+		}
+	}
+
 	void OnCollisionStay(Collision col) {
 		if (col.collider.attachedRigidbody != null) {
 			Rigidbody body = col.collider.attachedRigidbody;
 			//Vector3[] points = new Vector3[col.contacts.Length];
-
-			//for (int i = 0; i < points.Length; i++) {
-			//body.AddForceAtPosition(force / points.Length, points[i]);
-			//body.AddForce(force, ForceMode.Acceleration);
+			//for (int i = 0; i < col.contacts.Length; i++) {
+			//	points[i] = col.contacts[i].point;
 			//}
 
-			//body.velocity = Vector3.zero;
-			//body.AddForceAtPosition(motion, VectorHelper.Average(points), ForceMode.VelocityChange);
-
+			//body.AddForceAtPosition(motion, VectorHelper.Average(points), ForceMode.Acceleration);
+			//if (motion.MaxValue() > 0.1f)
+			//	print("Add force to " + body.name);
 			Vector3 vel = body.velocity;
 
 			if (motion.x != 0) vel.x = motion.x;
