9e98df5a29608d4da3fb3c757d013a943816e2f7
kalle (jag) <kalle.jillheden@me.com>
2016-04-27 11:19:29 +0200

    eahswarharf

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+	[SceneDropDown]
+	public int maneMenu;
+	
+	public void JumpToManeMenu() {
+		SceneManager.LoadScene(maneMenu);
+	}	
+
+}
