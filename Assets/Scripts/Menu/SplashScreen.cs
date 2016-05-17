4e6123481a6bce966dcd6dc1fa86de5f66cbb58a
kalle (jag) <kalle.jillheden@me.com>
2016-05-17 17:03:05 +0200

    Minor to major changes
    + Added random start delay to prefab spawners. Closes #25.
    + Added quicksave when picking up the fan. Closes #24.
    + Added approximation to cancel out float variable errors in the
    ConveyorController script. Fixes #27.
    + Now unequips coreitems directly when opening the HUD. Closes #26.
    + Devlog of HUD gizmos.
    + Reorginized some scripts into their belonging folders.
    + v1.0.4 -> v1.0.5

---
+++ Assets/Scripts/Menu/SplashScreen.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+	[SceneDropDown]
+	public int maneMenu;
+	
+	public void JumpToManeMenu() {
+		LoadingScreen.LoadRoom(maneMenu);
+		//SceneManager.LoadScene(maneMenu);
+	}	
+
+}
